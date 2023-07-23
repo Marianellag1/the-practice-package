569ba88446da6065fdbc3b365e846b82046a2ca5
Mgamboa <marianellag400@gmail.com>
2023-07-23 17:19:34 -0500

    started clock with javascript, will be adding extras

---
+++ clock-time/asset/js/script.js
@@ -0,0 +1,39 @@
+// console.log("hello world");// works
+let clock = document.getElementById("display-clock");
+
+function displayClock() {
+    const today = new Date(); //date has everything ie month, day, etc.
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+    // console.log(clock);
+    
+    //setting the AM and PM condition
+    let meridiem = 'AM';
+    if (hours >= 12) {
+        meridiem = 'PM'; 
+    }
+    
+    if (hours > 12) {
+        hours = hours - 12; //12 is sub. from hour since its 12hr time
+    }
+    
+    //if any are less than 10, add 0 infront of number
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    
+    clock.innerHTML = `${hours}:${minutes}:${seconds} ${meridiem}`
+}
+
+
+setInterval(displayClock, 1000);
+displayClock();
